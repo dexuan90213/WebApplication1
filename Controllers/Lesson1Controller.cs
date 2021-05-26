bd05d3089b5c0a88c751277d3e6161e0ea9a5ff9
Te-Hsuan <dexuan90213@outlook.com>
2021-05-26 13:21:51 +0800

    Lesson1 Controller

---
+++ Controllers/Lesson1Controller.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class Lesson1Controller : Controller
+    {
+        // GET: Lesson1
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
